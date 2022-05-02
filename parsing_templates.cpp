2d9f3327622410e2fe115d683095efc829c3d149
healytpk <thomaspkhealy@yahoo.com>
2022-05-02 12:20:35 +0100

    Very good progress on finding :: from the end of the string

--- parsing_templates.cpp
+++ parsing_templates.cpp
@@ -37,18 +37,17 @@ template<
     class CharT = typename std::iterator_traits<BidirIt>::value_type,
     class Traits = std::regex_traits<CharT>
 >
-class top_level_regex_token_iterator : regex_token_iterator<BidirIt,CharT,Traits> {
+class regex_top_level_token_iterator : std::regex_token_iterator<BidirIt,CharT,Traits> {
 private:
 
-    using Base = regex_token_iterator<BidirIt,CharT,Traits>;
+    using Base = std::regex_token_iterator<BidirIt,CharT,Traits>;
     Base &base = *static_cast<Base*>(this);
 
 protected:
 
-    BidirIt const _a, _b;
+    BidirIt const _a, _b;  // set in constructor's initialiser list
 
-    mutable bool _strided = false;
-    mutable Base::value_type _match{};
+    typename Base::value_type _strided_match;  /* starts off with matched = false */
 
     bool Is_Top_Level(void) const
     {
@@ -56,20 +55,19 @@ protected:
 
         size_t counts[4u] = {};  /* (), [], {}, <> */
 
-        std::sub_match<BidirIt> const &monkeyA = *base;
-
-        std::pair<BidirIt, BidirIt> const &monkeyB = monkeyA;
-
-        for ( BidirIt iter = _a; iter != monkeyB.second; ++iter )
+        for ( BidirIt iter = _a; iter != (*base).second; ++iter )
         {
             switch ( *iter )
             {
             case '(': ++(counts[0u]); break;
             case ')': --(counts[0u]); break;
+
             case '[': ++(counts[1u]); break;
             case ']': --(counts[1u]); break;
+
             case '{': ++(counts[2u]); break;
             case '}': --(counts[2u]); break;
+
             case '<': ++(counts[3u]); break;
             case '>': --(counts[3u]); break;
             }
@@ -85,89 +83,199 @@ protected:
 
 public:
 
-    top_level_regex_token_iterator(void) : Base(), _a(), _b() {}
+    regex_top_level_token_iterator(void) : Base(), _a(), _b() {}
 
-    top_level_regex_token_iterator(BidirIt a, BidirIt b,
-                                   regex_token_iterator<BidirIt,CharT,Traits>::regex_type const &re,
-                                   int submatch = 0,
-                                   std::regex_constants::match_flag_type m = std::regex_constants::match_default )
-    : Base(a,b,re,submatch, m), _a(a), _b(b)
+    void Keep_Searching_If_Necessary(void)
     {
-        for ( _strided = false; base != Base(); ++base)
+        for ( _strided_match.matched = false; base != Base(); ++base )
         {
             if ( this->Is_Top_Level() )
             {
-                break;
+                _strided_match.second = (*base).second;  // redundant when _strided_match.matched == false
+                return;
             }
             else
             {
-                if ( false == _strided )
+                if ( false == _strided_match.matched )
                 {
-                    _match.first = (*base).first;
-                    _strided = true;
+                    _strided_match.matched = true;
+
+                    _strided_match.first = (*base).first;
                 }
-            }            
+            }
         }
+
+        _strided_match.matched = false;
     }
 
-    top_level_regex_token_iterator &operator++(void)
+    regex_top_level_token_iterator(BidirIt const a, BidirIt const b,
+                                   typename Base::regex_type const &re,
+                                   int const submatch = 0,
+                                   std::regex_constants::match_flag_type const m = std::regex_constants::match_default )
+      : Base(a,b,re,submatch,m), _a(a), _b(b)
+    {
+        Keep_Searching_If_Necessary();
+    }
+
+    regex_top_level_token_iterator &operator++(void)
     {
         assert( base != Base() );  // operator++ should never be called on a "no more matches" token iterator
 
         ++base;
 
-        for ( _strided = false; base != Base(); ++base)
-        {
-            if ( this->Is_Top_Level() )
-            {
-                break;
-            }
-            else
-            {
-                if ( false == _strided )
-                {
-                    _match.first = (*base).first;
-                    _strided = true;
-                }
-            }            
-        }
+        Keep_Searching_If_Necessary();
 
         return *this;
     }
 
-    bool operator==(top_level_regex_token_iterator const &rhs) const
+    bool operator==(regex_top_level_token_iterator const &rhs) const  // Since C++20 we don't need operator!=
     {
         return base == rhs;
     }
 
-
-    Base::value_type const &operator*(void) const
+    typename Base::value_type const &operator*(void) const
     {
         assert( base != Base() );  // operator* should never be called on a "no more matches" token iterator
 
-        if ( false == _strided )
+        if ( false == _strided_match.matched )
         {
             return *base;
         }
 
-        _match.matched = true;
+        return _strided_match;
+    }
 
-        /* _match.first is already set */
-        _match.second = (*base).second;
+    typename Base::value_type const *operator->(void) const
+    {
+        assert( base != Base() );  // operator* should never be called on a "no more matches" token iterator
 
-        return _match;
+        if ( false == _strided_match.matched )
+        {
+            return base.operator->();
+        }
+
+        return &_strided_match;
     }
 };
 
-using svtop_level_regex_token_iterator = top_level_regex_token_iterator<std::string_view::const_iterator>;
+template<
+    class BidirIt,
+    class CharT = typename std::iterator_traits<BidirIt>::value_type,
+    class Traits = std::regex_traits<CharT>
+>
+class regex_top_level_iterator : std::regex_iterator<BidirIt,CharT,Traits> {
+private:
+
+    using Base = std::regex_iterator<BidirIt,CharT,Traits>;
+    Base &base = *static_cast<Base*>(this);
+
+protected:
+
+    BidirIt const _a, _b;  // set in constructor's initialiser list
+
+    bool Is_Top_Level(void) const
+    {
+        assert( base != Base() );  // Is_Top_Level should never be called on a "no more matches" token iterator
+
+        size_t counts[4u] = {};  /* (), [], {}, <> */
+
+        for ( BidirIt iter = _a; iter != (*base)[0u].first; ++iter )
+        {
+            switch ( *iter )
+            {
+            case '(': ++(counts[0u]); break;
+            case ')': --(counts[0u]); break;
+
+            case '[': ++(counts[1u]); break;
+            case ']': --(counts[1u]); break;
+
+            case '{': ++(counts[2u]); break;
+            case '}': --(counts[2u]); break;
+
+            case '<': ++(counts[3u]); break;
+            case '>': --(counts[3u]); break;
+            }
+        }
+
+        for ( auto const &count : counts )
+        {
+            if ( 0u != count ) return false;
+        }
+
+        return true;
+    }
+
+public:
+
+    regex_top_level_iterator(void) : Base(), _a(), _b() {}
+
+    void Keep_Searching_If_Necessary(void)
+    {
+        for ( ; base != Base(); ++base )
+        {
+            if ( this->Is_Top_Level() )
+            {
+                return;
+            }
+        }
+    }
+
+    regex_top_level_iterator(BidirIt const a, BidirIt const b,
+                             typename Base::regex_type const &re,
+                             std::regex_constants::match_flag_type const m = std::regex_constants::match_default )
+      : Base(a,b,re,m), _a(a), _b(b)
+    {
+        Keep_Searching_If_Necessary();
+    }
+
+    regex_top_level_iterator &operator++(void)
+    {
+        assert( base != Base() );  // operator++ should never be called on a "no more matches" token iterator
+
+        ++base;
+
+        Keep_Searching_If_Necessary();
+
+        return *this;
+    }
+
+    bool operator==(regex_top_level_iterator const &rhs) const  // Since C++20 we don't need operator!=
+    {
+        return base == rhs;
+    }
+
+    typename Base::value_type const &operator*(void) const
+    {
+        assert( base != Base() );  // operator* should never be called on a "no more matches" token iterator
+
+        return *base;
+    }
+
+    typename Base::value_type const *operator->(void) const
+    {
+        assert( base != Base() );  // operator* should never be called on a "no more matches" token iterator
+
+        return base.operator->();
+    }
+};
+
+using sregex_top_level_token_iterator    = regex_top_level_token_iterator<     string::const_iterator>;
+using svregex_top_level_token_iterator   = regex_top_level_token_iterator<string_view::const_iterator>;
+using sregex_top_level_iterator          = regex_top_level_iterator      <     string::const_iterator>;
+using svregex_top_level_iterator         = regex_top_level_iterator      <string_view::const_iterator>;
+
+using r_sregex_top_level_token_iterator  = regex_top_level_token_iterator<     string::const_reverse_iterator>;
+using r_svregex_top_level_token_iterator = regex_top_level_token_iterator<string_view::const_reverse_iterator>;
+using r_sregex_top_level_iterator        = regex_top_level_iterator      <     string::const_reverse_iterator>;
+using r_svregex_top_level_iterator       = regex_top_level_iterator      <string_view::const_reverse_iterator>;
 
 void PrintUsings(string_view const full_classname, string_view const unspecialised, string_view const specialised)
 {
     regex const my_separator(",");
-    svtop_level_regex_token_iterator it_unsp(unspecialised.cbegin(), unspecialised.cend(), my_separator, -1);
-    svtop_level_regex_token_iterator   it_sp(  specialised.cbegin(),   specialised.cend(), my_separator, -1);
+    svregex_top_level_token_iterator it_unsp(unspecialised.cbegin(), unspecialised.cend(), my_separator, -1);
+    svregex_top_level_token_iterator   it_sp(  specialised.cbegin(),   specialised.cend(), my_separator, -1);
 
-    for ( ; (it_unsp != svtop_level_regex_token_iterator()) && (it_sp != svtop_level_regex_token_iterator()); ++it_unsp, ++it_sp )
+    for ( ; (it_unsp != svregex_top_level_token_iterator()) && (it_sp != svregex_top_level_token_iterator()); ++it_unsp, ++it_sp )
     {
         string impersonator( *it_unsp );
 
@@ -200,21 +308,44 @@ void PrintUsings(string_view const full_classname, string_view const unspecialis
 
 int main(void)
 {
+
+#if 0
+
     string str("dog, cat, fish, (frogs,toads), monkeys, elephants, (lizards, amphibians<true,1>), sharks");
 
     cout << str << endl;
 
     regex const my_separator(",");
 
-    svtop_level_regex_token_iterator iter( string_view(str).cbegin(), string_view(str).cend(), my_separator, -1 );
+    svregex_top_level_token_iterator iter( string_view(str).cbegin(), string_view(str).cend(), my_separator, -1 );
     
-    for ( ; iter != svtop_level_regex_token_iterator(); ++iter )
+    for ( ; iter != svregex_top_level_token_iterator(); ++iter )
     {
         string tmp(*iter);
         boost::trim_all(tmp);
         cout << tmp << endl;
     }
-    
+
+#else
+
+    string str("::std::__allocator_traits_base::__rebind<_Tp,_Up,__void_t<typename _Tp::template rebind<_Up>::other>>::");
+
+    cout << str << endl;
+
+    regex const double_colon("::");
+
+    r_svregex_top_level_iterator iter( string_view(str).crbegin(), string_view(str).crend(), double_colon );
+
+    for ( ; iter != r_svregex_top_level_iterator(); ++iter )
+    {
+        // Since we have a reverse iterator, 'first' and 'second' are reversed, so use 'second' in the next line:
+        string tmp( &*(   ((*iter)[0u]).first   ) - 1u, &*(str.cend()) );  // REVISIT FIX dereference invalid pointer -- -1u is a bug improvisation
+        boost::trim_all(tmp);
+        cout << tmp << endl;
+    }
+
+#endif
+
     return 0;
 
     pair<string, string> classes[] = {

681f4fe6bb0f698f0fe9f68065c6003372bdda1b
healytpk <thomaspkhealy@yahoo.com>
2022-06-01 16:05:47 +0100

    Remove 3rd party libraries from Linux_run_cppcheck script

--- Linux_run_cppcheck.sh
+++ Linux_run_cppcheck.sh
@@ -4,13 +4,8 @@ rm -rf dir_cppcheck_*
 mkdir dir_cppcheck_work
 mkdir dir_cppcheck_html
 
-libraries_to_check=`find /usr/share/cppcheck/cfg ! -type d | xargs -i -r -n1 basename "{}" | rev | cut -d '.' -f 2- | rev | awk '{print " --library=" $s " "}' | tr -d '\n'`
-
-echo "Libraries to check: ${libraries_to_check}"
-
-# platforms to try: pic16, cray_sv1, arm32-wchar_t4, arm64-wchar_t4
-sh -c "\
-cppcheck $1 --platform=pic16 \
+# platforms to try: pic16, cray_sv1, arm32-wchar_t4, arm64-wchar_t4 (--platform=pic16)
+cppcheck $1 \
     --cppcheck-build-dir=./dir_cppcheck_work/ \
     --inline-suppr \
     --force \
@@ -18,7 +13,6 @@ cppcheck $1 --platform=pic16 \
     -v \
     --std=c++20 \
     --enable=all \
-    ${libraries_to_check}" \
     2> err.xml
 
 cppcheck-htmlreport --source-encoding="iso8859-1" --file=err.xml --report-dir=./dir_cppcheck_html/ --source-dir=./

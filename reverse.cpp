56521468ed7f6183780d2da036a54f36473609a7
healytpk <thomaspkhealy@yahoo.com>
2022-06-01 16:07:29 +0100

    Minor alterations to standalone program reverse.cpp

--- reverse.cpp
+++ reverse.cpp
@@ -3,75 +3,80 @@ namespace Continuity_Methods {
     typedef decltype(sizeof(char)) size_t;
     typedef decltype((char*)0 - (char*)0) ptrdiff_t;
 
-    template <typename T>
+    template<typename T>
     class reverse_iterator {
     protected:
 
-        T *current;
+        T *p;
 
     public:
 
-        bool operator!=(reverse_iterator const &rhs) const
+        constexpr bool operator!=(reverse_iterator const &rhs) const
         {
-            return current != rhs.current;
+            return p != rhs.p;
         }
 
-        constexpr reverse_iterator(T *arg) : current(arg) {}
+        constexpr reverse_iterator(T *arg) : p(arg) {}
 
-        constexpr T &operator*() const
+        constexpr T &operator*(void) const
         {
-            T *tmp = current;
+            T *tmp = p;
             return *--tmp;
         }
 
-        constexpr reverse_iterator &operator++()
+        constexpr reverse_iterator &operator++(void)
         {
-            --current;
+            --p;
             return *this;
         }
 
-        constexpr reverse_iterator &operator--()
+        constexpr reverse_iterator &operator--(void)
         {
-            ++current;
+            ++p;
             return *this;
         }
 
-        constexpr T &operator[](ptrdiff_t _Off) const
+        constexpr T &operator[](ptrdiff_t const arg) const
         {
-            return current[static_cast<ptrdiff_t>(-_Off - 1)];
+            return p[static_cast<ptrdiff_t>(-arg - 1)];
         }
     };
 
-    template <typename T>
-    struct reversion_wrapper { T& t; };
+    template<typename T,size_t N>
+    struct reversion_wrapper {
+        
+        T (&t)[N];
 
-    template <typename T, long unsigned N>
-    reverse_iterator<T> begin(reversion_wrapper<T[N]> arg)
-    {
-        return reverse_iterator<T>(arg.t + N);
-    }
+        constexpr reverse_iterator<T> begin(void)
+        {
+            return reverse_iterator<T>(t + N);
+        }
 
-    template <typename T, long unsigned N>
-    reverse_iterator<T> end(reversion_wrapper<T[N]> arg)
-    {
-        return reverse_iterator<T>(arg.t + 0u);
-    }
+        constexpr reverse_iterator<T> end(void)
+        {
+            return reverse_iterator<T>(t + 0u);
+        }
 
-    class reversed_order_t {} destructor_order;
+    };
 
-    template <typename T, long unsigned N>
-    reversion_wrapper<T[N]> operator|(T (&arg)[N], reversed_order_t)
+    class reversed_order_t {};
+
+    template<typename T, size_t N>
+    constexpr reversion_wrapper<T,N> operator|(T (&arg)[N], reversed_order_t)
     {
         return { arg };
     }
 
-    class intact_order_t {} constructor_order;
+    class intact_order_t {} ;
 
-    template <typename T, size_t N>
-    T (&operator|(T (&arg)[N], intact_order_t))[N]
+    template<typename T, size_t N>
+    constexpr T (&operator|(T (&arg)[N], intact_order_t))[N]
     {
         return arg;
     }
+
+    intact_order_t   constexpr constructor_order;
+    reversed_order_t constexpr destructor_order ;
 } // close nameapace
 
 #include <iostream>

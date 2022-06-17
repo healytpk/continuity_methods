d3880dd25fa3d706a48803e82be24f92996f1c50
healytpk <thomaspkhealy@yahoo.com>
2022-06-17 23:34:55 +0100

    New Linux script to build and test precompiler with Intel icpc

---
+++ build_for_intel.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+rm precompiler donkey*
+icpc -o precompiler ./precompiler.cpp -std=c++17
+icpc -E -P sample_lasers.cpp | ./precompiler > donkey.cpp
+icpc -o donkey ./donkey.cpp -std=c++17
+./donkey

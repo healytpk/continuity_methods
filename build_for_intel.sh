0135f1a49c715c4ab4a6b8b7fe22643be8599044
healytpk <thomaspkhealy@yahoo.com>
2022-06-17 23:38:45 +0100

    Script to build and test Intel icpc compiler now builds sample_simple too

--- build_for_intel.sh
+++ build_for_intel.sh
@@ -4,3 +4,8 @@ icpc -o precompiler ./precompiler.cpp -std=c++17
 icpc -E -P sample_lasers.cpp | ./precompiler > donkey.cpp
 icpc -o donkey ./donkey.cpp -std=c++17
 ./donkey
+
+rm donkey*
+icpc -E -P sample_simple.cpp | ./precompiler > donkey.cpp
+icpc -o donkey ./donkey.cpp -std=c++17
+./donkey

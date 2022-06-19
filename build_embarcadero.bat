164cf3213f75f7349aef88cdd67c8117ae3ef9a7
TPK Healy <thomaspkhealy@yahoo.com>
2022-06-19 23:43:24 +0100

    Embarcadero preprocessor output fed into precompiler seems to be working
    But watch out for public::std (obviously a parsing error somewhere)

--- build_embarcadero.bat
+++ build_embarcadero.bat
@@ -1,1 +1,16 @@
-"C:\Program Files\Embarcadero\RAD_Studio_11\bin\bcc64" -o precompiler.exe -std=c++17 precompiler.cpp import64.a cw64mt.a
+del precompiler.exe trans_*.cpp final_*.cpp bin_*.exe
+
+set CXX="C:\Program Files\Embarcadero\RAD_Studio_11\bin\bcc64"
+
+%CXX% -o precompiler.exe -std=c++17 precompiler.cpp import64.a cw64mt.a
+
+
+%CXX% -o trans_lasers.cpp -E -P sample_lasers.cpp
+type trans_lasers.cpp | precompiler.exe > final_lasers.cpp
+%CXX% -o bin_lasers.exe -std=c++17 final_lasers.cpp import64.a cw64mt.a
+bin_lasers.exe
+
+%CXX% -o trans_simple.cpp -E -P sample_simple.cpp
+type trans_simple.cpp | precompiler.exe > final_simple.cpp
+%CXX% -o bin_simple.exe -std=c++17 final_simple.cpp import64.a cw64mt.a
+bin_simple.exe

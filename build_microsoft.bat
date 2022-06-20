96a6c3b4f92dc798a823e354df2c38f162b14aad
TPK Healy <thomaspkhealy@yahoo.com>
2022-06-20 09:12:30 +0100

    Microsoft MSVC compiler batch file now tests precompiler

--- build_microsoft.bat
+++ build_microsoft.bat
@@ -3,3 +3,11 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Bu
 set compiler="C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Tools\MSVC\14.32.31326\bin\Hostx64\x64\cl.exe"
 
 %compiler% /Fe:precompiler.exe ./precompiler.cpp /std:c++20 /EHsc
+
+%compiler% /EP sample_simple.cpp /std:c++20 | precompiler.exe > trans_simple.cpp
+%compiler% /Fe:bin_simple.exe ./trans_simple.cpp /std:c++20 /EHsc
+bin_simple.exe
+
+%compiler% /EP sample_simple.cpp /std:c++20 | precompiler.exe > trans_simple.cpp
+%compiler% /Fe:bin_simple.exe ./trans_simple.cpp /std:c++20 /EHsc
+bin_simple.exe

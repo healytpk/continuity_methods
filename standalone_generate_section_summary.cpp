089a268301fcbc316fc314a6487cb911c07c8492
healytpk <thomaspkhealy@yahoo.com>
2022-06-28 15:45:05 +0100

    Update to standalone program for pretty printing of summary banner

--- standalone_generate_section_summary.cpp
+++ standalone_generate_section_summary.cpp
@@ -23,46 +23,15 @@ int main(int const argc, char **const argv)
 
         ++total_count_lines;
 
-        if ( string_view(line).contains("Section") )
+        if ( string_view(line).contains("Section") && (false == string_view(line).starts_with(" * ")) )
         {
-            records.push_back( pair<unsigned,string>(i, line) );
+            records.push_back( pair<unsigned,string>(i - 1u, line) );
         }
     }
 
     for ( unsigned i = 0; i != records.size(); ++i )
     {
         pair<unsigned,string> &e = records[i];
-
-        cout << " * (Section ";
-
-        if ( i+1u < 10u ) cout << ' ';
-
-        cout << i+1u << ") Line #";
-
-        if ( e.first < 1000u ) cout << ' ';        
-        if ( e.first <  100u ) cout << ' ';
-        if ( e.first <   10u ) cout << ' ';
-
-        cout << e.first << " - Line #";
-
-        if ( (i + 1u) < records.size() )
-        {
-            pair<unsigned,string> &e = records[i + 1u];
-
-            if ( e.first-2u < 1000u ) cout << ' ';        
-            if ( e.first-2u <  100u ) cout << ' ';
-            if ( e.first-2u <   10u ) cout << ' ';
-
-            cout << e.first-2u;
-        }
-        else
-        {
-            if ( total_count_lines < 1000u ) cout << ' ';        
-            if ( total_count_lines <  100u ) cout << ' ';
-            if ( total_count_lines <   10u ) cout << ' ';
-
-            cout << total_count_lines;
-        }
 
         string const &line = e.second;
 
@@ -72,8 +41,60 @@ int main(int const argc, char **const argv)
         {
             index = line.find(' ', index) + 1u;
         }
-    
-        cout << " : " << line.substr(index) << endl;
+
+        cout << " * (Section ";
+
+        if ( i+1u < 10u ) cout << ' ';
+
+        cout << i+1u << ") ";
+
+        cout << line.substr(index);
+
+        for ( unsigned i = 0u; i < (57u - line.substr(index).size()); ++i ) cout << " ";
+
+        cout << " : Line #";
+
+        if ( e.first < 1000u ) cout << ' ';        
+        if ( e.first <  100u ) cout << ' ';
+        if ( e.first <   10u ) cout << ' ';
+
+        unsigned a = e.first;
+        unsigned b;
+
+        cout << e.first << " - #";
+
+        if ( (i + 1u) < records.size() )
+        {
+            pair<unsigned,string> &e = records[i + 1u];
+
+            b = e.first-2u;
+
+            if ( e.first-2u < 1000u ) cout << ' ';        
+            if ( e.first-2u <  100u ) cout << ' ';
+            if ( e.first-2u <   10u ) cout << ' ';
+
+            cout << e.first-1u;
+        }
+        else
+        {
+            b = total_count_lines;
+
+            if ( total_count_lines < 1000u ) cout << ' ';        
+            if ( total_count_lines <  100u ) cout << ' ';
+            if ( total_count_lines <   10u ) cout << ' ';
+
+            cout << total_count_lines;
+        }
+
+        string blah = std::to_string(b-a + 2u);
+
+        cout << " : (Total = ";
+
+        if ( blah.size() < 4u ) cout << ' ';
+        if ( blah.size() < 3u ) cout << ' ';
+        if ( blah.size() < 2u ) cout << ' ';
+
+        cout << blah << " lines)" << endl;
     }
 
     return 0;

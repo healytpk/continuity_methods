bdd35cced29686b54932b0e4648da81b2c6f08c7
healytpk <thomaspkhealy@yahoo.com>
2022-07-01 23:03:48 +0100

    New script run_thomas_egypt.sh (works independently of Github Actions)

---
+++ run_thomas_egypt.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -rf 3rdparty/egypt/work/*
+find 3rdparty/egypt/ -iname "*\.egypt" -or -iname "*\.dot" -or -iname "*\.png" -or -iname "*\.svg" | xargs -i -r -n1 rm -rf "{}"
+find 3rdparty/egypt/ -mindepth 1 -maxdepth 1 -type l | xargs -i -r -n1 rm -rf "{}"
+
+cat .github/workflows/linux_g++.yml | grep run | tail -2 | cut -d ' ' -f 8- | tr -d '#' | xargs -i -r -n1 sh -c "{}"
+xdg-open ./3rdparty/egypt/main_as_root.png &
